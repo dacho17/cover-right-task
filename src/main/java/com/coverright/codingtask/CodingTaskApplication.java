1326650e714aff24b9010f700cec498057b7c832
dacho <dosenovicdavid7@gmail.com>
2023-01-16 07:41:38 +0100

    Project Initialized

---
+++ src/main/java/com/coverright/codingtask/CodingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.coverright.codingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coverright/codingtask/CodingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverright.codingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
